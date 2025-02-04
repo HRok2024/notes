30864c3b32b477c586016e54040dd4c4298ea85a
rok <rok@gmail.com>
2025-02-04 14:57:41 +0900

    컨트롤러 테스트

--- src/main/java/com/secure/notes/controllers/HelloController.java
+++ src/main/java/com/secure/notes/controllers/HelloController.java
@@ -1,4 +1,27 @@
 package com.secure.notes.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+    @GetMapping("/contact")
+    public String sayContact(){
+        return "contact World";
+    }
+    @GetMapping("/hi")
+    public String sayHi(){
+        return "hi World";
+    }
+    
+    @GetMapping("/public/abc")
+    public String sayPublic(){
+        return "인증필요없음";
+    }
 }
