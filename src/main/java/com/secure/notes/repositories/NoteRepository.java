531090b52884019c97a0690e914be05d6efd607e
rok <rok@gmail.com>
2025-02-05 09:47:26 +0900

    노트 서비스 구현

--- src/main/java/com/secure/notes/repositories/NoteRepository.java
+++ src/main/java/com/secure/notes/repositories/NoteRepository.java
@@ -3,5 +3,9 @@ package com.secure.notes.repositories;
 import com.secure.notes.models.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NoteRepository extends JpaRepository<Note, Long> {
+    //메서드 이름만 규칙에 맞춰서 인터페이스를 구현하는 느낌으로만 만들어두면 알아서 구현이 된다
+    List<Note> findByOwnerUserName(String username);
 }
---
+++ src/main/java/com/secure/notes/services/impl/NoteServiceImpl.java
@@ -0,0 +1,45 @@
+package com.secure.notes.services.impl;
+
+import com.secure.notes.models.Note;
+import com.secure.notes.repositories.NoteRepository;
+import com.secure.notes.services.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NoteServiceImpl implements NoteService {
+    @Autowired
+    private NoteRepository noteRepository;
+
+    @Override
+    public Note createNoteForUser(String username, String content) {
+        Note note = new Note();
+        note.setContent(content);
+        note.setOwnerUsername(username);
+        Note savedNote = noteRepository.save(note);
+        return savedNote;
+    }
+
+    @Override
+    public Note updateNoteForUser(Long noteId, String content, String username) {
+        Note note=noteRepository.findById(noteId).orElseThrow(()->new RuntimeException("Note not found"));
+        note.setContent(content);
+        //노트 객체에 id가 포함되어 있으므로 저장이 아닌 업데이트가 된다
+        Note updatedNote=noteRepository.save(note);
+        return updatedNote;
+    }
+
+    @Override
+    public void deleteNoteForUser(Long noteId, String username) {
+        noteRepository.deleteById(noteId);
+
+    }
+
+    @Override
+    public List<Note> getNotesForUser(String username) {
+        List<Note> personalNotes=noteRepository.findByOwnerUserName(username);
+        return personalNotes;
+    }
+}
